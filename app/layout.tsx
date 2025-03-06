b1c8dd2f789737a46a82e54b7f482506f808f7f9
Injoon Oh <injoon5@naver.com>
2025-03-06 16:21:17 +0900

    font pretendard

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
+import { Analytics } from "@vercel/analytics/react"
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -18,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="ko" suppressHydrationWarning>
       <body className={`${inter.className} dark:bg-neutral-900 dark:text-white`}>{children}</body>
+      <Analytics />
     </html>
   )
 }
